a81c8e2889053e366447861efacdf4bac863b569
dannykrug <dannykrug@gmail.com>
2019-02-02 19:01:41 -0500

    header updated

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Header } from './components/common'
+
+App = () => {
+  return (
+  <View>
+    <Header headerText='ParrotMeet' />
+  </View>
+  )
+}
+
+export default App;
