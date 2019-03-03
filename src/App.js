224222723bdfe34ee338b7b00b456924d213c7c6
dannykrug <dannykrug@gmail.com>
2019-03-03 15:47:24 -0500

    login form up

--- src/App.js
+++ src/App.js
@@ -6,18 +6,32 @@ App = () => {
   return (
   <View>
     <Header headerText='ParrotMeet' />
-    <Input placeholder="username">Login</Input>
     <Card>
       <Image style={styles.imageStyle} source={{uri: 'https://i.pinimg.com/originals/db/38/a1/db38a1d743bddc0ad7d6c6d9b3760008.jpg'}} />
       <CardSection>
+        <Text>Login</Text>
+      </CardSection>
+
+      <CardSection>
+        <Input placeholder="username" />
+      </CardSection>
+
+      <CardSection>
+        <Input placeholder="password" secureTextEntry />
+      </CardSection>
+
+      <CardSection>
         <Button>News Feed</Button>
       </CardSection>
+
       <CardSection>
         <Button>Concert Feed</Button>
       </CardSection>
+
       <CardSection>
         <Button>My Concerts</Button>
       </CardSection>
+
       <CardSection>
         <Button>Log Out</Button>
       </CardSection>
